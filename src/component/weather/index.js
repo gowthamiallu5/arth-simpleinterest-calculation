21d2007c64fc189813731f93e72ecd8c9382b69b
Anish Singh <anishs293@outlook.com>
2020-09-21 21:27:12 +0530

    added weather section

---
+++ src/component/weather/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.scss";
+
+//function component as an arrow function
+const Weather = () => {
+  const handle = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="main-cont">
+      <form onSubmit={handle}>
+        <h1>Weather</h1>
+        <input type="text" placeholder="Enter City" maxLength="50" value="" />
+        <button type="submit">Get Forecast</button>
+        <div className="weather-info">
+          <p>City : {}</p>
+          <p>Date : {}</p>
+          <p>Temperature : {}</p>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Weather;
