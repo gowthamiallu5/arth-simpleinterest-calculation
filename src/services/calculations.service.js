86dbcdc1992e4e22586c45342e365f4bcd107ffa
gowthamiallu5 <gowthami.allu@augmentolabs.com>
2020-10-06 17:15:18 +0530

    ga-arthemetic integration

--- src/services/calculations.service.js
+++ src/services/calculations.service.js
@@ -12,51 +12,68 @@ export const calculationService = {
 };
 
 async function addition(data) {
-    //     const data ={
-    //     "a":1,
-    //     "b":2
-    //   }
-    await axios.post(API_URL + 'api/Operations/add', data)
-        .then((response) => {
-            console.log(response);
+    console.log('addition',data);
 
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( data)
+    };
+
+    return fetch(API_URL + 'api/Operations/add', requestOptions)
+        .then(handleResponse)
+        .then(response => {
+            console.log('inservice', response);
             return response;
-        }).catch((error) => {
-            console.log(error)
-        })
+        });
 }
 
 async function substration(data) {
-    await axios.post(API_URL + 'api/Operations/add', data)
-        .then((response) => {
-            console.log(response);
+   
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( data)
+    };
 
+    return fetch(API_URL + 'api/Operations/substract', requestOptions)
+        .then(handleResponse)
+        .then(response => {
+            console.log('inservice', response);
             return response;
-        }).catch((error) => {
-            console.log(error)
-        })
+        });
 }
 
 async function multiplication(data) {
-    await axios.post(API_URL + 'api/Operations/add', data)
-        .then((response) => {
-            console.log(response);
+   
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( data)
+    };
 
+    return fetch(API_URL + 'api/Operations/multiply', requestOptions)
+        .then(handleResponse)
+        .then(response => {
+            console.log('inservice', response);
             return response;
-        }).catch((error) => {
-            console.log(error)
-        })
+        });
 }
 
 async function division(data) {
-    await axios.post(API_URL + 'api/Operations/add', data)
-        .then((response) => {
-            console.log(response);
+   
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( data)
+    };
 
+    return fetch(API_URL + 'api/Operations/divide', requestOptions)
+        .then(handleResponse)
+        .then(response => {
+            console.log('inservice', response);
             return response;
-        }).catch((error) => {
-            console.log(error)
-        })
+        });
 }
 
 async function simpleinterest(data) {
