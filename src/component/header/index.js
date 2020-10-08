d81391156bf087ea7a61b0097cbd3dc87c28c66a
gowthamiallu5 <gowthami.allu@augmentolabs.com>
2020-10-08 11:45:56 +0530

    ga

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -21,13 +21,13 @@ const Header = (props) => {
                 <Link to="/">Arithmetic</Link>
               </li>
               <li>
-                <Link to="/#/Simpleinterest">Simple Interest</Link>
+                <Link to="/Simpleinterest">Simple Interest</Link>
               </li>
               {/* <li>
                 <Link to="/Dummyapi">Dummy api</Link>
               </li> */}
               <li>
-                <Link to="/#/Weather">Temperature</Link>
+                <Link to="/Weather">Temperature</Link>
               </li>
             </ul>
             <div className="content">
